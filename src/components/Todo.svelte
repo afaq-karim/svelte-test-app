3415546878d66348ddeaa9c887eb4964b59b197e
afaq-karim <ashfaqhunzai@gmail.com>
2023-06-02 01:55:13 +0500

    created todo app

---
+++ src/components/Todo.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let title: string;
+  export let completed: boolean;
+
+  onMount(() => {
+    console.log(title, completed);
+  })
+</script>
+
+<style>
+  .container {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+  }
+
+  h2 {
+    margin: 0;
+  }
+
+  .completed {
+    text-decoration: line-through;
+    opacity: 0.5;
+  }
+</style>
+
+<div class="container">
+  <h2 class:completed={completed}>{title}</h2>
+  {#if completed}
+    <input type="checkbox" name="todo" id={title} value={true} checked>
+  {:else}
+    <input type="checkbox" name="todo" id={title} value={true}>
+  {/if}
+</div>
+
+
+
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Todo from "../components/Todo.svelte";
+
+  // define todo items 
+  let todos = [
+    { id: 1, title: 'Task 1', completed: false },
+    { id: 2, title: 'Task 2', completed: true }
+  ]
+</script>
+
+<style>
+
+</style>
+
+<div>
+  { #each todos as todo (todo.id) }
+  <Todo title={todo.title} completed={todo.completed} />  
+  {/each}
+</div>
